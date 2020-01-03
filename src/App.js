8abe729842330fd94afb2b85dde711a1c41aaa57
Ben Taylor <bentaylor@bentaylor.me>
2020-01-02 22:55:57 -0600

    added mlh banner

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Team from './components/team';
 import What from './components/what';
 import Footer from './components/footer';
 import Sponsors from './components/sponsors';
+import MLHBanner from './components/mlh-banner';
 
 function App() {
   return (
     <div className="App">
+      <MLHBanner />
       <Landing />
       <What />
       <FAQ />
---
+++ src/components/mlh-banner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MLHBanner = (props) => (
+   <a 
+      href='https://mlh.io/seasons/na-2020/events'
+      style={{
+         position: 'fixed',
+         top: 0,
+         right: '10vw',
+         width: 100
+      }}
+   >
+      <img src='/images/mlh-banner.svg' alt='Major League Hacking 2020 Season Official Event' />
+   </a>
+)
+
+export default MLHBanner;
