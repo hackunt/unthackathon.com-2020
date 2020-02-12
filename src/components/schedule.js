c9f7ba6d90f01850bea6175e4aa16db8052b141b
Ben Taylor <bentaylor@bentaylor.me>
2020-02-12 14:42:18 -0600

    added AA workshop, moved CTF

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -8,13 +8,13 @@ const Schedule = (props) => (
       <h3>Saturday</h3>
       <ul>
          {
-            schedule.slice(0, 10).map(v => <li className={v.title.match(/mlh|workshop/i) ? "workshop" : v.title.match(/lunch|dinner|breakfast|snack/i) ? "food" : ""}><span className='time'>{v.start}</span>{v.title}</li>)
+            schedule.slice(0, 11).map(v => <li className={v.title.match(/mlh|workshop/i) ? "workshop" : v.title.match(/lunch|dinner|breakfast|snack/i) ? "food" : ""}><span className='time'>{v.start}</span>{v.title}</li>)
          }
       </ul>
       <h3>Sunday</h3>
       <ul>
          {
-            schedule.slice(10).map(v => <li className={v.title.match(/mlh|workshop/i) ? "workshop" : v.title.match(/lunch|dinner|breakfast|snack/i) ? "food" : ""}><span className='time'>{v.start}</span>{v.title}</li>)
+            schedule.slice(11).map(v => <li className={v.title.match(/mlh|workshop/i) ? "workshop" : v.title.match(/lunch|dinner|breakfast|snack/i) ? "food" : ""}><span className='time'>{v.start}</span>{v.title}</li>)
          }
       </ul>
    </div> 
